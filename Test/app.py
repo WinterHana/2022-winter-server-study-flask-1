92835c68d31f88e0f443e2debc6f7adc8170e5af
WinterHana <haha920@naver.com>
2023-02-14 15:41:22 +0900

    new

--- Test/app.py
+++ Test/app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션
 api = Api(app)  # Flask 객체에 Api 객체 등록
 
 
-@api.route('/hello')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
+@api.route('/hello/<string:name>')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class HelloWorld(Resource):
-    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
-        return {"hello": "world!"}
+    def get(self, name):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
+        return {"message": "Welcome, %s" % name}
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
